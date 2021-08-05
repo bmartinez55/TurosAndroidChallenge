1076e58d3f9c761048855445263ced505076d998
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2021-08-05 12:33:56 -0700

    Added temp logo if business doesn't have a image_url.

--- app/src/main/java/c/bmartinez/yelpclone/ui/views/MainActivity.kt
+++ app/src/main/java/c/bmartinez/yelpclone/ui/views/MainActivity.kt
@@ -1,23 +1,8 @@
 package c.bmartinez.yelpclone.ui.views
 
-import android.app.SearchManager
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.SearchView
-import android.widget.Toast
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import c.bmartinez.yelpclone.R
-import c.bmartinez.yelpclone.data.model.Results
-import c.bmartinez.yelpclone.data.repo.YelpRepository
-import c.bmartinez.yelpclone.data.services.RetrofitService
-import c.bmartinez.yelpclone.ui.viewmodels.MainViewModel
-import c.bmartinez.yelpclone.ui.viewmodels.MyViewModelFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,54 +12,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mainFragment: MainFragment = MainFragment()
+        val mainFragment = MainFragment()
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.main_frag_container, mainFragment).commitAllowingStateLoss()
     }
-
-    //override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-//        this.menu = menu!!
-//        menuInflater.inflate(R.menu.main_menu, menu)
-//
-//        val searchItem: MenuItem = menu.findItem(R.id.search)
-//        val searchView: androidx.appcompat.widget.SearchView = searchItem.actionView as androidx.appcompat.widget.SearchView
-//
-//        searchView.setOnQueryTextListener(object: androidx.appcompat.widget.SearchView.OnQueryTextListener {
-//            override fun onQueryTextSubmit(input: String?): Boolean {
-//                if(!input.isNullOrBlank()){
-//                    searchData(input)
-//                    return false
-//                }
-//                return true
-//            }
-//            override fun onQueryTextChange(p0: String?): Boolean {
-//                return true
-//            }
-//
-//        })
-//        return true
-    //}
-
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        when(item.itemId) {
-//            R.id.search -> return true
-//        }
-//        return super.onOptionsItemSelected(item)
-//    }
-//
-//    private fun searchData(searchTerm: String){
-//        viewModel.getSearchResults(searchTerm)
-//        viewModel.data.observe(this, {
-//            if(it.isEmpty()){
-//                Log.d(TAG, "Search came back empty")
-//            } else {
-//                if(data.isNotEmpty()){
-//                    data.clear()
-//                }
-//                data.addAll(it)
-//                adapter.notifyDataSetChanged()
-//            }
-//        })
-//    }
 
 }
--- app/src/main/java/c/bmartinez/yelpclone/ui/views/ResultsAdapter.kt
+++ app/src/main/java/c/bmartinez/yelpclone/ui/views/ResultsAdapter.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.RecyclerView
 import c.bmartinez.yelpclone.R
 import c.bmartinez.yelpclone.data.model.Results
@@ -28,7 +30,9 @@ class ResultsAdapter (private val context: Context, private val data: List<Resul
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         fun bind(p: Results){
             itemView.findViewById<TextView>(R.id.itemTitle).text = p.name
-            Glide.with(context).load(p.image_url).into(itemView.findViewById(R.id.itemImage))
+            if(p.image_url.isNullOrEmpty() || p.image_url.isBlank()){
+                itemView.findViewById<AppCompatImageView>(R.id.itemImage).setBackgroundResource(R.drawable.temp_business_icon)
+            } else { Glide.with(context).load(p.image_url).into(itemView.findViewById(R.id.itemImage)) }
             val address = p.location.address + " " + p.location.city + ", " + p.location.state + " " + p.location.zipCode
             itemView.findViewById<TextView>(R.id.itemAddress).text = address
             val decFormat = DecimalFormat("#.##")
