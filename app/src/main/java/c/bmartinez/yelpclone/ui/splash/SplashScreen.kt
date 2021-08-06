b75dd6c2d070fcd2f720321c5944762dcc3a9baf
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2021-08-06 11:39:57 -0700

    Check location permissions in splash screen.

--- app/src/main/java/c/bmartinez/yelpclone/ui/splash/SplashScreen.kt
+++ app/src/main/java/c/bmartinez/yelpclone/ui/splash/SplashScreen.kt
@@ -1,40 +1,78 @@
 package c.bmartinez.yelpclone.ui.splash
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.*
 import android.util.Log
-import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import c.bmartinez.yelpclone.R
 import c.bmartinez.yelpclone.ui.views.MainActivity
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import androidx.core.widget.ImageViewCompat as ImageViewCompat1
 
 class SplashScreen: AppCompatActivity() {
 
     val TAG = SplashScreen::class.java.name
+
+    companion object {
+        private const val MY_PERMISSIONS_REQUEST_LOCATION = 99
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
         Log.d(TAG, "Inside onCreate()")
         Log.d(TAG, "After setting the content view")
+
+        checkLocationPermissions()
     }
 
     override fun onResume() {
         super.onResume()
         Log.d(TAG, "Inside onResume()")
-        Log.d(TAG, "Before calling GlobalScope")
-        val mContext = this.applicationContext
-        GlobalScope.launch(Dispatchers.IO) {
-            Handler(Looper.getMainLooper()).postDelayed({
-                Log.d(TAG, "Inside the handler")
-                val intent = Intent(mContext, MainActivity::class.java)
-                startActivity(intent)
-                finish()
-            }, 10000)
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            startMainActivity()
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_LOCATION -> {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                        startMainActivity()
+                    }
+                } else {
+                    Toast.makeText(this, "Permission Denied: certain features will not work properly", Toast.LENGTH_SHORT).show()
+                    startMainActivity()
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun checkLocationPermissions(){
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                    MY_PERMISSIONS_REQUEST_LOCATION
+                )
+            }
+        }
+    }
+
+    private fun startMainActivity(){
+        Log.d(TAG, "Before switching to MainActivity")
+        val intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
+        finish()
     }
 }
