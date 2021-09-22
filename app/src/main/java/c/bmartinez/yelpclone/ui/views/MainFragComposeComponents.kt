6dc9a9f704c984b281fa415a4323529d5893ecb6
Brian Martinez <brianrmartinezcastaneda@csumb.edu>
2021-09-22 10:27:36 -0700

    Progress: Learning how to create a horizontal grid layout with compose.

--- app/src/main/java/c/bmartinez/yelpclone/ui/views/MainFragComposeComponents.kt
+++ app/src/main/java/c/bmartinez/yelpclone/ui/views/MainFragComposeComponents.kt
@@ -1,19 +1,24 @@
 package c.bmartinez.yelpclone.ui.views
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement.Vertical
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import c.bmartinez.yelpclone.R
 import c.bmartinez.yelpclone.data.model.Results
+import com.skydoves.landscapist.CircularReveal
 import com.skydoves.landscapist.glide.GlideImage
 
 class MainFragComposeComponents {
@@ -54,15 +59,16 @@ class MainFragComposeComponents {
                 .padding(5.dp),
             elevation = 10.dp
         ) {
-            Column(
-
-            ) {
-                GlideImage(
-                    imageModel = popularLocation.image_url,
-                    contentScale = ContentScale.Inside,
-                    circularRevealEnabled = true
-                )
-            }
+            GlideImage(
+                imageModel = popularLocation.image_url,
+                contentScale = ContentScale.Inside,
+                circularReveal = CircularReveal(duration = 250),
+                placeHolder = ImageBitmap.imageResource(R.drawable.temp_business_icon),
+                error = ImageBitmap.imageResource(R.drawable.temp_business_icon)
+            )
+            Column(verticalArrangement = Vertical)
         }
     }
+
+
 }
